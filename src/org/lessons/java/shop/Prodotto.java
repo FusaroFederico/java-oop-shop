0b4e88e8a54cb50a0b124039a096fba08325c9c3
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-15 12:09:24 +0200

    add instance variables and constructor method to Prodotto.java

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,24 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	// instance variables
+	private int itemCode;
+	private String itemName;
+	private String itemDescription;
+	private double itemPrice;
+	private int itemVat;
+	// constructor method
+	public Prodotto(String itemName, String itemDescription, double itemPrice,int itemVat) {
+		// new instance for random number
+		Random random = new Random();
+		// 8-digit random number
+		this.itemCode = 1 + random.nextInt(99999999);
+		
+		this.itemName = itemName;
+		this.itemDescription = itemDescription;
+		this.itemPrice = itemPrice;
+		this.itemVat = itemVat;
+	}
 }
