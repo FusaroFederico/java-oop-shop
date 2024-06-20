a1edd530daa9f8b55ba2039d512a6b8d21fb4077
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-20 16:44:40 +0200

    Update Television.java
    Add instance variables, constructor method, getters and setters

--- src/org/lessons/java/shop/Television.java
+++ src/org/lessons/java/shop/Television.java
@@ -1,5 +1,32 @@
 package org.lessons.java.shop;
 
 public class Television extends Prodotto{
+	// instance variables
+	int tvDimension;
+	boolean isSmart;
+	
+	// constructor method
+	public Television(String itemName, String itemDescription, double itemPrice, int itemVat, int tvDimension, boolean isSmart) {
+		super(itemName, itemDescription, itemPrice, itemVat);
+		this.tvDimension = tvDimension;
+		this.isSmart = isSmart;
+	}
+	
+	// Getters and Setters
+	public int getTvDimension() {
+		return tvDimension;
+	}
+
+	public void setTvDimension(int tvDimension) {
+		this.tvDimension = tvDimension;
+	}
+
+	public boolean isSmart() {
+		return isSmart;
+	}
+
+	public void setSmart(boolean isSmart) {
+		this.isSmart = isSmart;
+	}
 	
 }
