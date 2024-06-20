faa3a95ab7c4465fc1314fbf70f1f513cd3aac44
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-20 16:50:56 +0200

    Update Cuffie.java
    add instance variables, constuctor method, getters and setters

--- src/org/lessons/java/shop/Cuffie.java
+++ src/org/lessons/java/shop/Cuffie.java
@@ -1,5 +1,33 @@
 package org.lessons.java.shop;
 
 public class Cuffie extends Prodotto{
+	// instance variables
+	String coluor;
+	boolean isWireless;
+	
+	// constructor method
+	public Cuffie(String itemName, String itemDescription, double itemPrice, int itemVat, String colour, boolean isWireless) {
+		super(itemName, itemDescription, itemPrice, itemVat);
+		this.coluor = colour;
+		this.isWireless = isWireless;
+	}
+	
+	// Getters and Setters
+	public String getColuor() {
+		return coluor;
+	}
+
+	public void setColuor(String coluor) {
+		this.coluor = coluor;
+	}
+
+	public boolean isWireless() {
+		return isWireless;
+	}
+
+	public void setWireless(boolean isWireless) {
+		this.isWireless = isWireless;
+	}
+	
 	
 }
