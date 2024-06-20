cd1720303f8b489b1799d19f7d77708112b865c4
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-20 16:36:55 +0200

    add instance variables, constructor method, getters and setters to Smartphone.java

--- src/org/lessons/java/shop/Smartphone.java
+++ src/org/lessons/java/shop/Smartphone.java
@@ -1,5 +1,29 @@
 package org.lessons.java.shop;
 
 public class Smartphone extends Prodotto{
+	// instance variables
+	long phoneIMEICode;
+	int phoneStorage;
+	
+	// constructor method
+	public Smartphone(String itemName, String itemDescription, double itemPrice, int itemVat, long phoneIMEICode, int phoneStorage) {
+		super(itemName, itemDescription, itemPrice, itemVat);
+		this.phoneIMEICode = phoneIMEICode;
+		this.phoneStorage = phoneStorage;
+	}
+	
+	// Getters and Setters
+	public long getPhoneIMEICode() {
+		return phoneIMEICode;
+	}
+	public void setPhoneIMEICode(long phoneIMEICode) {
+		this.phoneIMEICode = phoneIMEICode;
+	}
+	public int getPhoneStorage() {
+		return phoneStorage;
+	}
+	public void setPhoneStorage(int phoneStorage) {
+		this.phoneStorage = phoneStorage;
+	}
 	
 }
